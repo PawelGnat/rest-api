cdefe325daab5f922bcc9041e5977e9237422fdd
Pawel <p.gnat@artneo.pl>
2024-07-04 12:52:43 +0200

    fix: api folder

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,92 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import compression from "compression";
-import http from "http";
-import cors from "cors";
-import mongoose from "mongoose";
-import cron from "node-cron";
-import "dotenv/config";
-import { Server } from "socket.io";
+// import express from "express";
+// import bodyParser from "body-parser";
+// import cookieParser from "cookie-parser";
+// import compression from "compression";
+// import http from "http";
+// import cors from "cors";
+// import mongoose from "mongoose";
+// import cron from "node-cron";
+// import "dotenv/config";
+// import { Server } from "socket.io";
 
-import router from "./router";
+// import router from "./router";
 
-import { getClients, settleAllClients } from "./db/clients";
-import { getUsers } from "./db/users";
+// import { getClients, settleAllClients } from "./db/clients";
+// import { getUsers } from "./db/users";
 
-const MONGO_URL = `${process.env.DATABASE_URL}`;
-const ORIGIN_URL = `${process.env.ORIGIN_URL}`;
+// const MONGO_URL = `${process.env.DATABASE_URL}`;
+// const ORIGIN_URL = `${process.env.ORIGIN_URL}`;
 
-const app = express();
+// const app = express();
 
-app.use(
-  cors({
-    origin: ORIGIN_URL,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ORIGIN_URL,
+//     credentials: true,
+//   })
+// );
 
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
+// app.use(compression());
+// app.use(cookieParser());
+// app.use(bodyParser.json());
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: ORIGIN_URL,
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-
-  socket.on("sendClients", async () => {
-    const clients = await getClients();
-    io.emit("clients", clients);
-  });
-
-  socket.on("sendUsers", async () => {
-    const users = await getUsers();
-    io.emit("users", users);
-  });
-});
-
-server.listen(8080, () => {
-  console.log("Server is running on http://localhost:8080/");
-});
-
-mongoose
-  .connect(MONGO_URL)
-  .then(() => {
-    console.log("Connected to MongoDB - on connect");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
-
-const db = mongoose.connection;
-
-db.on("error", (error) => {
-  console.error("MongoDB connection error:", error);
-});
-
-db.once("open", () => {
-  console.log("Connected to MongoDB - on open, once");
-});
-
-db.on("disconnected", () => {
-  console.log("Disconnected from MongoDB");
-});
-
-app.use("/", router());
-
-// cron.schedule("* * * * *", async () => {
-//   await settleAllClients();
-//   const clients = await getClients();
-//   io.emit("clients", clients);
-//   console.log("cron runed");
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: ORIGIN_URL,
+//   },
 // });
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+
+//   socket.on("sendClients", async () => {
+//     const clients = await getClients();
+//     io.emit("clients", clients);
+//   });
+
+//   socket.on("sendUsers", async () => {
+//     const users = await getUsers();
+//     io.emit("users", users);
+//   });
+// });
+
+// server.listen(8080, () => {
+//   console.log("Server is running on http://localhost:8080/");
+// });
+
+// mongoose
+//   .connect(MONGO_URL)
+//   .then(() => {
+//     console.log("Connected to MongoDB - on connect");
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
+
+// const db = mongoose.connection;
+
+// db.on("error", (error) => {
+//   console.error("MongoDB connection error:", error);
+// });
+
+// db.once("open", () => {
+//   console.log("Connected to MongoDB - on open, once");
+// });
+
+// db.on("disconnected", () => {
+//   console.log("Disconnected from MongoDB");
+// });
+
+// app.use("/", router());
+
+// // cron.schedule("* * * * *", async () => {
+// //   await settleAllClients();
+// //   const clients = await getClients();
+// //   io.emit("clients", clients);
+// //   console.log("cron runed");
+// // });
