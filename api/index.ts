dbb89104ce076842686fca4e97c5bc44747579a1
Pawel <p.gnat@artneo.pl>
2024-07-04 16:04:37 +0200

    fix

--- api/index.ts
+++ api/index.ts
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: `${ORIGIN_URL}:${ORIGIN_PORT}`,
+    origin: `${ORIGIN_URL}`,
     credentials: true,
   })
 );
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: `${ORIGIN_URL}:${ORIGIN_PORT}`,
+    origin: `${ORIGIN_URL}`,
   },
 });
 
