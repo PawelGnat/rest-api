1978fe60cf5674476d35cb1bdec6fdd37ad36c9d
Pawel <p.gnat@artneo.pl>
2024-07-04 13:53:09 +0200

    test

--- api/router/main.ts
+++ api/router/main.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 export default (router: express.Router) => {
   router.get("/", (req, res) => {
-    res.send("Server is running");
+    res.send("Server is running, origin url: " + process.env.ORIGIN_URL);
   });
 };
