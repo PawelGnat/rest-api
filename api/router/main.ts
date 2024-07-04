73e085fd960c95e0d3a186e6dd493e004bb20e7f
Pawel <p.gnat@artneo.pl>
2024-07-04 14:22:16 +0200

    fix

--- api/router/main.ts
+++ api/router/main.ts
@@ -3,7 +3,9 @@ import express from "express";
 export default (router: express.Router) => {
   router.get("/", (req, res) => {
     res.send(
-      `Server is running, Server is connected with ${process.env.ORIGIN_URL}:${process.env.ORIGIN_PORT}`
+      `Server is running, Server is connected with ${
+        process.env.ORIGIN_URL || "localhost"
+      }:${process.env.ORIGIN_PORT || 3000}`
     );
   });
 };
