c08145d7b1837d128570bcd3633bd475dfb5d2fe
Pawel <p.gnat@artneo.pl>
2024-07-10 14:06:29 +0200

    test

--- api/controllers/cron.ts
+++ api/controllers/cron.ts
@@ -1,8 +1,16 @@
+import express from "express";
+
 import { settleAllClients } from "../db/clients";
 
-export const cronSettleClients = async () => {
-  await settleAllClients();
-  // const clients = await getClients();
-  // io.emit("clients", clients);
-  console.log("cron runed");
+export const cronSettleClients = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    await settleAllClients();
+    return res.status(200).json("cron runed");
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
 };
